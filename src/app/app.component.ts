af497ac19e1dfa15afca678c78d24203af14a1b5
meansh <anshdotcontact.com>
2020-09-14 17:55:34 +0530

    A counter app, increase and decrease count.

---
+++ src/app/app.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent {
+  title: string = 'Counter App';
+
+  count: number = 0;
+
+  handleIncrease = () => {
+    if(this.count < 10)
+      this.count = this.count + 1;
+    else if(this.count >= 10)
+    this.count = 0;
+  }
+
+  handleDecrease = () => {
+    if(this.count > 0)
+    this.count = this.count - 1;
+    else if(this.count <= 0)
+    this.count = this.count - 0;
+  }
+
+  handleReset = () => {
+    this.count = 0;
+  }
+}
